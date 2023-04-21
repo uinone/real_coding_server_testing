6868ec5ac659871ce7cccef8639f61a3a8ff35c9
inspire12 <ox4443@naver.com>
2023-04-21 20:37:46 +0900

    feat(testing/week1-base) - test별 코드 설명 추가

--- src/test/java/com/cnu/real_coding_server/service/week1/base/service/PostServiceTest.java
+++ src/test/java/com/cnu/real_coding_server/service/week1/base/service/PostServiceTest.java
@@ -8,13 +8,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Description;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PostServiceTest {
 
-
-
+    @Description("프로젝트와 연관없이 독립적으로 실행")
     @DisplayName("테스트 코드 실행해보기")
     @Test
     void testDoCode() {
--- src/test/java/com/cnu/real_coding_server/service/week1/base/service/PostServiceWithSpringTest.java
+++ src/test/java/com/cnu/real_coding_server/service/week1/base/service/PostServiceWithSpringTest.java
@@ -9,12 +9,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Description;
 
 @SpringBootTest
 public class PostServiceWithSpringTest {
+
+
     @Autowired
     PostService postService;
 
+    @Description("프로젝트에서 테스트 해볼 모듈을 가져와서 독립적으로 실행")
     @DisplayName("테스트 코드에서 Spring Component 실행해보기")
     @Test
     void testDoCodeRun() {
